1318573b9d8ad5670465eac672b03219894a2be6
HyowonKim <33456790+kimyo1@users.noreply.github.com>
2022-12-11 22:58:33 +0900

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  const topBtn = document.getElementById("top-btn");
-  topBtn.addEventListener("click", function () {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  });
-};
