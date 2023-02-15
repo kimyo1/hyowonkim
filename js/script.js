1005de2740a241a420eb9ce0813d066193565310
HyowonKim <33456790+kimyo1@users.noreply.github.com>
2023-02-15 13:56:10 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.onload = function () {
   const topBtn = document.getElementById("top-btn");
   topBtn.addEventListener("click", function () {
     window.scrollTo({
-      top: 0,
+      top: 100,
       behavior: "smooth",
     });
   });
